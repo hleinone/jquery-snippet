fc0ee8d12129ddbb08677c7a22f8efc61dfe1bce
Hannu Leinonen <hleinone@gmail.com>
2011-05-26 12:09:48 +0300

    Removed an unnecessary function.

--- jquery.snippet.js
+++ jquery.snippet.js
@@ -7,17 +7,14 @@
  *   http://www.gnu.org/licenses/gpl.html
  */
 
-/**
- * jQuery.snippet
- * 
- * Easily create HTML snippets and stores them in the snippet element's
- * parent element's data by the snippet element's id. Get the snippet
- * by $("#parent").snippet("snippetId")
- */
 (function($) {
-  $.fn.snippet = function() {
-  }
-  
+  /**
+   * jQuery.snippet
+   * 
+   * Easily create HTML snippets and stores them in the snippet element's
+   * parent element's data by the snippet element's id. Get the snippet
+   * by $("#parent").snippet("snippetId")
+   */
   $.fn.snippet = function(snippetId) {
     if (typeof snippetId === "undefined") {
       return this.each(function(index, element) {
